be47e9362c6d1ce1a0686ca4716f00cb551725c2
maudro <maudro29@gmail.com>
2019-10-17 08:22:48 +0300

    finished

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -3,18 +3,30 @@ class GuessingGame {
 
     setRange(min, max) {
 
+         this.min=min;
+         this.max=max;
+
+         this.a=[];
+         for (var i=0; i<this.max-this.min+1; i++) {
+             this.a[i]=this.min+i;
+          } 
     }
 
     guess() {
 
+         this.mid=(this.min+this.max)/2;
+         if (this.mid>parseInt(this.mid)) this.mid=this.mid+0.5; 
+         return this.a[this.mid];
     }
 
     lower() {
 
+         this.max=this.mid;
     }
 
     greater() {
 
+          this.min=this.mid;
     }
 }
 
